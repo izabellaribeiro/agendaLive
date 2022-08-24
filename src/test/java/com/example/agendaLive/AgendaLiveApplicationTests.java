1d0f405e085351cc073c878caed61e77a716e404
izabellaribeiro <izabella.ribeirro@gmail.com>
2022-08-24 17:06:11 -0300

    primeiro commit e inicialização do projeto

---
+++ src/test/java/com/example/agendaLive/AgendaLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendaLive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
